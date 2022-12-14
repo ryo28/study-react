e71c43aa64395b0a2c205e3cce2044fbd0c226ea
ryo28 <101950814+ryo28@users.noreply.github.com>
2022-12-15 02:43:38 +0900

    ✏️ Linksの修正ミス

--- components/Links.js
+++ components/Links.js
@@ -3,23 +3,23 @@ import styles from "./Links.module.css";
 const ITEMS = [
   {
     href: "https://nextjs.org/docs",
-    title: "Documentation &rarr;",
+    title: "Documentation →",
     description: "Find in-depth information about Next.js features and API.",
   },
   {
     href: "https://nextjs.org/learn",
-    title: "Learn &rarr;",
+    title: "Learn →",
     description: "Learn about Next.js in an interactive course with quizzes!",
   },
   {
     href: "https://github.com/vercel/next.js/tree/canary/examples",
-    title: "Examples &rarr;",
+    title: "Examples →",
     description: "Discover and deploy boilerplate example Next.js projects.",
   },
   {
     href:
       "https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app",
-    title: "Deploy &rarr;",
+    title: "Deploy →",
     description:
       "Instantly deploy your Next.js site to a public URL with Vercel.",
   },
