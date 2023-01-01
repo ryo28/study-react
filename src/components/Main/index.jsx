ba5ee94a5b33c53540819da5f682b9a905dc3815
ryo28 <101950814+ryo28@users.noreply.github.com>
2023-01-01 23:56:13 +0900

    🏗️ 拡張子・ディレクトリ構成・インポート方法の変更、_appを用いた処理共通化

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,6 +1,6 @@
-import styles from "./Main.module.css";
-import { Headline } from "../Headline";
-import { Links } from "../Links";
+import styles from "src/components/Main/Main.module.css";
+import { Headline } from "src/components/Headline";
+import { Links } from "src/components/Links";
 
 export function Main(props) {
   return (
