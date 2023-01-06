48b564b1980d83d2727bd9ceaf7f01f0c10dbc0a
ryo28 <101950814+ryo28@users.noreply.github.com>
2023-01-06 16:34:19 +0900

    ✨講座用のuseEffectを導入

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,16 +3,28 @@ import styles from "../styles/Home.module.css";
 import { Footer } from "../components/Footer";
 import { Main } from "../components/Main";
 import { Header } from "../components/Header";
-import { useCallback } from "react";
+import { useCallback, useEffect } from "react";
+
 
 export default function Home() {
-const foo = 1;
+  const foo = 1;
 
-  const handleClick = useCallback((e) => {
-    console.log(e.target.href);
-    e.preventDefault();
-    alert(foo);
+    const handleClick = useCallback((e) => {
+      console.log(e.target.href);
+      e.preventDefault();
+      alert(foo);
+    }, []);
+
+  useEffect(() => {
+    console.log("マウント時");
+    document.body.style.backgroundColor = "lightblue";
+    return () => {
+      console.log("アンマウント時");
+
+      document.body.style.backgroundColor = "";
+    };
   }, []);
+  
   return (
     <div className={styles.container}>
       <Head>
