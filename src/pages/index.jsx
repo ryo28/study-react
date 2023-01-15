6ac79535d33ac3419bb0c8c059efccdbadd7aad6
ryo28 <101950814+ryo28@users.noreply.github.com>
2023-01-15 20:12:24 +0900

    ✨ useStateで配列を扱ってみた

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -9,6 +9,7 @@ export default function Home() {
   const [count, setCount] = useState(1);
   const [text, setText] = useState("");
   const [isShow, setIsShow] = useState(true);
+  const [array, setArray] = useState([]);
 
   const handleClick = useCallback(() => {
     if (count < 10) {
@@ -27,6 +28,17 @@ export default function Home() {
     }
     setText(e.target.value.trim());
   }, []);
+  const handleAdd = useCallback(() => {
+    setArray((prevArray) => {
+      if (prevArray.some(item => item === text)) {
+        alert("同じ要素がすでに存在します")
+        return prevArray;
+      }
+      return [...prevArray, text];
+      
+      return newArray;
+    });
+  }, [text]);
 
   useEffect(() => {
     document.body.style.backgroundColor = "lightblue";
@@ -48,6 +60,12 @@ export default function Home() {
 
       <button onClick={handleDisplay}>{isShow ? "非表示" : "表示"}</button>
       <input type="text" value={text} onChange={handelChange} />
+      <button onClick={handleAdd}>追加</button>
+      <ul>
+        {array.map((item) => {
+          return <li key={item}>{item}</li>;
+        })}
+      </ul>
       <Main page="index" />
       <Footer />
     </div>
