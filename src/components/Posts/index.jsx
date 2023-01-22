a57fddcb8ea04a86461a27017c48407a121f0bf2
ryo28 <101950814+ryo28@users.noreply.github.com>
2023-01-23 02:06:56 +0900

    ⚡️ useReducerを使いました！

--- src/components/Posts/index.jsx
+++ src/components/Posts/index.jsx
@@ -1,9 +1,26 @@
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect, useReducer } from "react";
+
+const initialState = {
+  date: [],
+  loading: true,
+  error: null,
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "end":
+      return { ...state, date: action.date, loading: false };
+
+    case "error":
+      return { ...state, loading: false, error: action.error };
+    default: {
+      throw new Error("no  such action type!");
+    }
+  }
+};
 
 export const Posts = () => {
-  const [posts, setPosts] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   const getPosts = useCallback(async () => {
     try {
@@ -12,31 +29,31 @@ export const Posts = () => {
         throw new Error("エラーが発生したため、データが取得に失敗しました。");
       }
       const json = await res.json();
-      setPosts(json);
+      dispatch({ type: "end", date: json });
     } catch (error) {
-      setError(error);
+      dispatch({ type: "error", error });
     }
-
-    setLoading(false);
   }, []);
 
   useEffect(() => {
     getPosts();
   }, [getPosts]);
 
-  if (loading) {
+  console.log("foo");
+
+  if (state.loading) {
     return <div>ローディング中</div>;
   }
-  if (error) {
-    return <div>{error.message}</div>;
+  if (state.error) {
+    return <div>{state.error.message}</div>;
   }
-  if (posts.length === 0) {
+  if (state.date.length === 0) {
     return <div>データは空です</div>;
   }
 
   return (
     <ol>
-      {posts.map((post) => {
+      {state.date.map((post) => {
         return <li key={post.id}>{post.title}</li>;
       })}
     </ol>
