3c4d73dd1674b61a691250c17963092bd9451571
ryo28 <101950814+ryo28@users.noreply.github.com>
2023-02-06 18:55:27 +0900

    とりあえずコミットまだ途中

--- src/components/Posts/index.jsx
+++ src/components/Posts/index.jsx
@@ -1,59 +1,35 @@
-import { useCallback, useEffect, useReducer } from "react";
+import useSWR from "swr";
 
-const initialState = {
-  date: [],
-  loading: true,
-  error: null,
+const fetcher = async (url) => {
+  const response = await fetch(url);
+
+if (!response.ok) {
+  throw new Error("エラーが発生したため、データの取得に失敗しました。");
+}
+const json = await response.json();
+return json;
 };
-
-const reducer = (state, action) => {
-  switch (action.type) {
-    case "end":
-      return { ...state, date: action.date, loading: false };
-
-    case "error":
-      return { ...state, loading: false, error: action.error };
-    default: {
-      throw new Error("no  such action type!");
-    }
-  }
-};
-
 export const Posts = () => {
-  const [state, dispatch] = useReducer(reducer, initialState);
+  const { data, error, isLoading } = useSWR(
+    "https://jsonplaceholder.typicode.com/posts",
+    fetcher
+  );
 
-  const getPosts = useCallback(async () => {
-    try {
-      const res = await fetch("https://jsonplaceholder.typicode.com/posts");
-      if (!res.ok) {
-        throw new Error("エラーが発生したため、データが取得に失敗しました。");
-      }
-      const json = await res.json();
-      dispatch({ type: "end", date: json });
-    } catch (error) {
-      dispatch({ type: "error", error });
-    }
-  }, []);
+  console.log({ data, error });
 
-  useEffect(() => {
-    getPosts();
-  }, [getPosts]);
-
-  console.log("foo");
-
-  if (state.loading) {
+  if (isLoading) {
     return <div>ローディング中</div>;
   }
-  if (state.error) {
-    return <div>{state.error.message}</div>;
+  if (error) {
+    return <div>{error.message}</div>;
   }
-  if (state.date.length === 0) {
+  if (data.length === 0) {
     return <div>データは空です</div>;
   }
 
   return (
     <ol>
-      {state.date.map((post) => {
+      {data.map((post) => {
         return <li key={post.id}>{post.title}</li>;
       })}
     </ol>
